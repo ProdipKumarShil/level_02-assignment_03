a78eed7a11bf7100c7b7767ec297ee93c3043d30
ProdipKumarShil <prodipkrishna01@gmail.com>
2026-06-22 09:04:14 +0600

    added query - 7

--- query.sql
+++ query.sql
@@ -119,3 +119,8 @@ inner join users on users.user_id = bookings.user_id
 -- Query 6: Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings.
 select booking_id, match_id, total_cost from bookings
 where total_cost > (select avg(total_cost) from bookings)
+
+-- Query 7: Retrieve the top 2 most expensive matches sorted by base ticket price, skipping the absolute highest premium match.
+select match_id, fixture, base_ticket_price from matches
+order by base_ticket_price desc
+limit 4 offset 1
